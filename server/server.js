64e0868a100d813638de645c68e06cbb424617a0
Abhinav Bhattarai <69339222+Abhinav-Bhattarai@users.noreply.github.com>
2022-06-22 10:40:56 +0545

    Server Setup

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import https from "https";
+import fs from "fs";
+import cors from 'cors';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const config = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
+const server = https.createServer(config, app);
+
+// middleware
+app.use(cors({
+    credentials: true,
+    origin: ['https://localhost:3000','https://192.168.0.104:3000']
+}))
+
+// db connection
+mongoose.connect(process.env.URI).then(() => {
+    console.log('connected to mongoDB')
+}).catch(() => {
+    console.log('did not connect to mongoDB')
+})
+
+// listener
+server.listen(PORT, () => {
+  console.log(`Connected to localhost:${PORT}`);
+});
