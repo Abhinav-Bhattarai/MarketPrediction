19091aba606ef2824c8d1568ca76d4ef09476a36
Abhinav Bhattarai <69339222+Abhinav-Bhattarai@users.noreply.github.com>
2022-06-24 15:37:50 +0545

    FE: SETUPS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import useAuthorizationVerification from './Hooks/useAuthorizationVerification';
+import React from "react";
+import useAuthorizationVerification from "./Hooks/useAuthorizationVerification";
+import Button from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const authenticationStatus = useAuthorizationVerification();
-  console.log(authenticationStatus);
+  console.log(authenticationStatus)
 
   return (
     <React.Fragment>
-
+      <Button variant="primary" size="lg">Hello</Button>
     </React.Fragment>
   );
 }
